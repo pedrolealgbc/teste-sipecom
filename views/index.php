3c285ea299fccfbd6c4d78e43691c1db6392ce22
pedrolealgbc <33103084+pedrolealgbc@users.noreply.github.com>
2017-10-28 16:19:37 -0300

    Initial commit

---
+++ views/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>SIPECOM - Estamos testando nosso conhecimento</title>
+    <link rel="icon" href="../images/favicon.ico">
+    <meta name="description" content="No momento, estamos apenas testando nossos conhecimentos em linguagem e códigos de programação direcionada à web. Em breve estaremos desenvolvendo websites de alta qualidade.">
+    <meta name="author" content="SIPECOM">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../libraries/stylesheet.css">
+    <script src="../libraries/jquery-3.2.1.min.js"></script>
+    <script src="../libraries/javascript.js"></script>
+
+</head>
+
+<body>
+<header>
+    <div class="ind-topo"></div>
+    <nav>
+        <ul>
+            <li>Cadastro</li>
+            <li>Contato</li>
+            <li>Serviços</li>
+            <li>Equipe</li>
+            <li>Sobre</li>
+        </ul>
+    </nav>
+
+    <div class="ind-botao">
+        <img src="../images/botao.png" alt="Menu Mobile" title="Menu Mobile">
+    </div>
+
+    <div class="ind-logo">
+        <picture>
+            <source media="(max-width: 480px)" srcset="../images/smart-logo.png">
+            <source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/tablet-logo.png">
+            <source media="(min-width: 769px)" srcset="../images/dt-logo.png">
+            <img src="../images/smart-logo.png" alt="Menu Mobile" title="Menu Mobile">
+        </picture>
+    </div>
+
+
+</header>
+
+<main>
+
+
+</main>
+
+<footer>
+
+</footer>
+</body>
+
+
+</html>
